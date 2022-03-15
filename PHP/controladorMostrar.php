6dadc1df77d9f478c0e83670170c2c8212663ab2
legolas60007 <91493754+legolas60007@users.noreply.github.com>
2022-03-15 21:32:15 +0100

    Update controladorMostrar.php

--- PHP/controladorMostrar.php
+++ PHP/controladorMostrar.php
@@ -24,7 +24,7 @@ $username = "root";
 $password = "secret";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=iaw_db", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=tienda", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
